3b0e2ed881696cc00575de2c05a1d8b7068075a7
BenjamimFG <ben.FreitasG@gmail.com>
2021-03-31 03:46:36 -0300

    fix deprecated method assertThat change assertThat method import from junit (deprecated since junit 4.13) to hamcrest

--- src/test/java/br/com/aula/ContaTest.java
+++ src/test/java/br/com/aula/ContaTest.java
@@ -2,7 +2,7 @@ package br.com.aula;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.Test;
 
